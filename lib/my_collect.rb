242f7782fe22e9c5e7edda804e9e6bbcd804b6b3
Renee Deka Ambia <dekadekadeka@gmail.com>
2019-04-04 03:54:46 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -4,12 +4,9 @@ def my_collect(words)
   first = []
   while i < words.length
     yield words[i]
-    for elems in words[i]
-      if elems.length == 4
       first << words[i].split(" ").first
 #    up << words[i].upcase
     end
-  end
     i = i + 1
   end
   return first
