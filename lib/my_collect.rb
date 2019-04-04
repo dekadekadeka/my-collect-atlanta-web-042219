1c2837269ebcf8557263bc5e2bfb4b116cd4bbb7
Renee Deka Ambia <dekadekadeka@gmail.com>
2019-04-04 03:06:10 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,9 +2,8 @@ def my_collect(words)
   i = 0
   while i < words.length
     yield words[i]
-    words[i]
-    words_cap = words.upcase
+    words.upcase[i]
     i = i + 1
   end
-  return words_cap
+  return words
 end
